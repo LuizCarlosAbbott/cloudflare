b803b4c9f899a045a190f68a8e2e5dd23a0b77f8
LuizCarlosAbbott <luizcarlos_abbott@hotmail.com>
2020-04-20 21:58:05 -0300

    teste

--- my-worker/index.js
+++ my-worker/index.js
@@ -6,7 +6,7 @@ addEventListener('fetch', (event) => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response('Hello my ssh worker!', {
+  return new Response('Hello my sshworker!', {
     headers: { 'content-type': 'text/plain' },
   })
 }
